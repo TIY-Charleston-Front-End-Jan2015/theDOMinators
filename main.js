7479ae4e672224c2b184ef2842b2be15479000d2
Adam Sitz <sitzadad@Adams-MacBook-Pro.local>
2015-01-31 14:21:58 -0500

    translating is working both ways

--- main.js
+++ main.js
@@ -1,54 +1,84 @@
-var methods = {
-  init:function(){
-    methods.initStyle();
-  },
-  initStyle:function(){
-    $('#contentWrapper').hide();
-    methods.initEvents();
-  },
-  initEvents:function(){
-    $(window).one('scroll',methods.whenScroll);
-  //  $('body').on('mousedown',methods.whenClick);
-  },
-  whenScroll:function(){
-    $('h1').css({'font-size':'7em','margin-bottom':'0.6em'});
-    $('#splashWrapper').hide();
-    $('#contentWrapper').show('0.2s','linear');
-  },
-  // whenClick:function(){
-  //   if(){
-  //     methods.singleClick();
-  //   }else if(){
-  //     methods.doubleClick();
-  //   }else if(){
-  //     methods.tripleCLick();
-  //   }else if(){
-  //     methods.holdClick();
-  //   }
-  //},
-  singleClick:function(){
-
-  },
-  doubleClick:function(){
-
-  },
-  tripleClick:function(){
-
-  },
-  holdClick:function(){
-
-  }
-}
-$(document).ready(function(){
-  methods.init();
-});
-
 var mArray = [];
 var outputArray = [];
 var transArray =[];
 var assWords=[];
 var assCode=[];
 var finishArray = [];
+
+//function to break apart each word
+function splitIntoLetters(word){
+  var lettersArray = word.split('');
+  mArray.push(lettersArray);
+}
+function mAssign(lettersArray){
+  transArray =[];
+  _.each(lettersArray,mTranslate);
+  outputArray.push(transArray);
+}
+function mTranslate(letter){
+  for(i=0;i<alphaArray.length;i++){
+    if(letter===alphaArray[i]){
+      letter=morseArray[i];
+    transArray.push(letter);
+    }
+  }
+}
+
+function joinLetters(lettersArray) {
+  lettersArray =lettersArray.join(' ');
+
+  assWords.push(lettersArray);
+}
+
+
+
+//user input
+//var initInput = prompt('What would you like to translate?').toLowerCase();
+//splitting user input into array of words
+//var mInput = initInput.split(' ');
+//execute a function on each word in array
+//_.each(mInput,splitIntoLetters);
+//
+//_.each(mArray,mAssign);
+
+//_.each(outputArray,joinLetters);
+
+//alert(assWords.join('//'));
+
+
+//morse to English
+//var initMorse = prompt("What would you like to translate? Please type Morse code below.");
+//var initMorse = 'hellow';
+//var morseIn = initMorse.split(' ');
+
+
+//_.each(morseIn, cTranslate);
+//transArray= [];
+function cTranslate (letter) {
+  for(i=0; i<morseArray.length; i++) {
+    if (letter ===morseArray[i]) {
+      letter = alphaArray[i];
+      transArray.push(letter);
+      console.log(transArray);
+    }
+  }
+}
+
+//finishArray.push(transArray);
+
+//_.each(finishArray, joinCode);
+//console.log(finishArray);
+
+function joinCode (array) {
+  array= array.join('');
+  assCode.push(array);
+  console.log(assCode);
+}
+
+//alert (assCode.join('//'));
+
+
+
 var alphaArray = [
 'a',
 'b',
@@ -85,7 +115,8 @@ var alphaArray = [
 '6',
 '7',
 '8',
-'9'
+'9',
+' '
 ];
 var morseArray = [
 '.-',
@@ -124,75 +155,92 @@ var morseArray = [
 '--...',
 '---..',
 '----.',
+'//'
 ];
 
-//function to break apart each word
-function splitIntoLetters(word){
-  var lettersArray = word.split('');
-  mArray.push(lettersArray);
-}
-function mAssign(lettersArray){
-  transArray =[];
-  _.each(lettersArray,mTranslate);
-  outputArray.push(transArray);
-}
-function mTranslate(letter){
-  for(i=0;i<alphaArray.length;i++){
-    if(letter===alphaArray[i]){
-      letter=morseArray[i];
-    transArray.push(letter);
-    }
+var methods = {
+  init:function(){
+    methods.initStyle();
+  },
+  initStyle:function(){
+    $('#contentWrapper').hide();
+    methods.initEvents();
+  },
+  initEvents:function(){
+    $(window).one('scroll',methods.whenScroll);
+    $('#encodeButton').on('click',methods.doEncode);
+    $('#decodeButton').on('click',methods.doDecode);
+  //  $('body').on('mousedown',methods.whenClick);
+  },
+  whenScroll:function(){
+    $('h1').css({'font-size':'7em','margin-bottom':'0.6em'});
+    $('#splashWrapper').hide();
+    $('#contentWrapper').show('0.2s','linear');
+  },
+  // whenClick:function(){
+  //   if(){
+  //     methods.singleClick();
+  //   }else if(){
+  //     methods.doubleClick();
+  //   }else if(){
+  //     methods.tripleCLick();
+  //   }else if(){
+  //     methods.holdClick();
+  //   }
+  //},
+  singleClick:function(){
+
+  },
+  doubleClick:function(){
+
+  },
+  tripleClick:function(){
+
+  },
+  holdClick:function(){
+
+  },
+  clearEnglish:function(){
+    $('#inputEnglish').val("");
+  },
+  clearMorse:function(){
+    $('#inputMorse').val("");
+  },
+  doEncode:function(){
+    //
+    mArray = [];
+    outputArray = [];
+    transArray =[];
+    assWords=[];
+    assCode=[];
+    finishArray = [];
+    //
+    var initInput = $('#inputEnglish').val().toLowerCase();
+    var mInput = initInput.split(' ');
+    _.each(mInput,splitIntoLetters);
+    _.each(mArray,mAssign);
+    _.each(outputArray,joinLetters);
+    methods.clearMorse();
+    $('#inputMorse').val(assWords.join(' // '));
+  },
+  doDecode:function(){
+    //
+    mArray = [];
+    outputArray = [];
+    transArray =[];
+    assWords=[];
+    assCode=[];
+    finishArray = [];
+    //
+    var initMorse = $('#inputMorse').val().toLowerCase();
+    var morseIn = initMorse.split(' ');
+    _.each(morseIn, cTranslate);
+    finishArray.push(transArray);
+    _.each(finishArray, joinCode);
+    methods.clearEnglish();
+    $('#inputEnglish').val(assCode.join(' // '));
   }
 }
-
-function joinLetters(lettersArray) {
-  lettersArray =lettersArray.join(' ');
-
-  assWords.push(lettersArray);
-}
-
-
-
-//user input
-var initInput = prompt('What would you like to translate?').toLowerCase();
-//splitting user input into array of words
-var mInput = initInput.split(' ');
-//execute a function on each word in array
-_.each(mInput,splitIntoLetters);
-//
-_.each(mArray,mAssign);
-
-_.each(outputArray,joinLetters);
-
-alert(assWords.join('//'));
-
-
-//morse to English
-var initMorse = prompt("What would you like to translate? Please type Morse code below.");
-var morseIn = initMorse.split(' ');
-
-
-_.each(morseIn, cTranslate);
-transArray= [];
-function cTranslate (letter) {
-  for(i=0; i<morseArray.length; i++) {
-    if (letter ===morseArray[i]) {
-      letter = alphaArray[i];
-      transArray.push(letter);
-      console.log(transArray);
-    }
-  }
-}
-
-finishArray.push(transArray);
-
-_.each(finishArray, joinCode);
-console.log(finishArray);
-
-function joinCode (array) {
-  array= array.join(' ');
-  assCode.push(array);
-  console.log(assCode);
-}
-
-alert (assCode.join('//'));
+$(document).ready(function(){
+  methods.init();
+});
